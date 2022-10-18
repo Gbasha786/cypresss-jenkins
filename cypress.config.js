5fecc1a88b308309d2d61f038231ab5528e7b492
Gbasha786 <97756779+Gbasha786@users.noreply.github.com>
2022-10-18 20:33:14 +0530

    Update and rename cypress.json to cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '2x4aoc',
+} 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('before:browser:launch', (browser = {}, launchOptions) => {
+  module.exports = (on, config) => {
+  on('before:browser:launch', (browser = {}, launchOptions) => {
     console.log(launchOptions.args)
 
     if (browser.name == 'chrome') {
       launchOptions.args.push('--disable-gpu')
     }
-
     return launchOptions
-    },
-  },
-});
+  })
+}
